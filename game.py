db634d2b773bd515a246f961148325959dafbf10
Slayaundre <ibnat.mlis@gmail.com>
2024-05-29 20:18:48 +0600

    main

--- game.py
+++ game.py
@@ -1,39 +1,37 @@
 import random
 
+def get_user_choice():
+    while True:
+        user_input = input("Type Rock/Paper/Scissors or Q to quit: ").lower()
+        if user_input in options or user_input == "q":
+            return user_input
+
 user_wins = 0
 computer_wins = 0
+draws = 0
 
-options = ["rock", "paper", "scissors"] 
+options = ["rock", "paper", "scissors"]
+beats = {"rock": "scissors", "paper": "rock", "scissors": "paper"}
 
 while True:
-    user_input = input("Type Rock/Paper/Scissors or Q to quit: ").lower()
+    user_input = get_user_choice()
     if user_input == "q":
         break
 
-    if user_input not in options:
-        continue
+    computer_pick = random.choice(options)
+    print(f"Computer picked {computer_pick}.")
 
-    random_number = random.randint(0, 2)
-    # rock: 0, paper: 1, scissors:2
-    computer_pick = options[random_number]
-    print("Computer picked", computer_pick + ".")
-
-    if user_input == "rock" and computer_pick == "scissors":
+    if user_input == computer_pick:
+        print("It's a draw!")
+        draws += 1
+    elif beats[user_input] == computer_pick:
         print("You won!")
         user_wins += 1
-        
-    elif user_input == "paper" and computer_pick == "rock":
-        print("You won!")
-        user_wins += 1
-
-    elif user_input == "scissors" and computer_pick == "paper":
-        print("You won!")
-        user_wins += 1
-
     else:
         print("You lost!")
         computer_wins += 1
 
-print("You won", user_wins, "times.")
-print("The computer won", computer_wins, "times.")
+print(f"You won {user_wins} times.")
+print(f"The computer won {computer_wins} times.")
+print(f"There were {draws} draws.")
 print("Goodbye!")
